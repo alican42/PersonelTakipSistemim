b938aa4c560fe9d523e7a7e8c2fbc0b14ef55399
ali <aliakkan4203@gmail.com>
2025-05-11 10:53:23 +0300

    Login giriş tassarımı yapıldı

--- PersonelTakipSistemi/Form1.Designer.cs
+++ PersonelTakipSistemi/Form1.Designer.cs
@@ -29,33 +29,112 @@
         private void InitializeComponent()
         {
             label1 = new Label();
-            button1 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            label4 = new Label();
+            label5 = new Label();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(307, 136);
+            label1.Location = new Point(12, 29);
             label1.Name = "label1";
-            label1.Size = new Size(67, 20);
-            label1.TabIndex = 1;
-            label1.Text = "ali akkan";
+            label1.Size = new Size(92, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Kullanıcı Adı";
             // 
-            // button1
+            // label2
             // 
-            button1.Location = new Point(453, 117);
-            button1.Name = "button1";
-            button1.Size = new Size(94, 29);
-            button1.TabIndex = 2;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 67);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Parola";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 116);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Yetki";
+            label3.Click += label3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(118, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(189, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(118, 67);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(189, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(115, 112);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(82, 24);
+            radioButton1.TabIndex = 5;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Yönetici";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(221, 114);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(86, 24);
+            radioButton2.TabIndex = 6;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Kullanıcı";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 168);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Giriş Hakkı";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(118, 168);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 20);
+            label5.TabIndex = 8;
+            label5.Text = "label5";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button1);
+            ClientSize = new Size(393, 240);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
             Text = " Test Project Screen";
@@ -64,7 +143,15 @@
         }
 
         #endregion
+
         private Label label1;
-        private Button button1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Label label4;
+        private Label label5;
     }
 }
--- PersonelTakipSistemi/Form1.cs
+++ PersonelTakipSistemi/Form1.cs
@@ -6,5 +6,10 @@ namespace PersonelTakipSistemi
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
